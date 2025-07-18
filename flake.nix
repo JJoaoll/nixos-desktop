f0c14330db6184f6d13b4e9704fe27272151b60b
JJoaoll <joaoduos@gmail.com>
2025-07-18 11:47:54 -0300

    flake: init

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    # The host with the hostname `my-nixos` will use this configuration
+    nixosConfigurations.my-nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+      ];
+    };
+  };
+}
